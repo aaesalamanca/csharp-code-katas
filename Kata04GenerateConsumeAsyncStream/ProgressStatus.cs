4eb4dc2d56d2fbd94c6e94c01e75d994e904c115
aaesalamanca <aaesalamanca@gmail.com>
2023-01-09 10:41:11 +0100

    feat: New kata to understand async streams

---
+++ Kata04GenerateConsumeAsyncStream/ProgressStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Kata04GenerateConsumeAsyncStream;
+
+internal class ProgressStatus : IProgress<int>
+{
+    Action<int> _action;
+
+    public ProgressStatus(Action<int> action) => _action = action;
+
+    public void Report(int value) => _action(value);
+}
